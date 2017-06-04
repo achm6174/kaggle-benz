639df85664ea18279b10c397c1f57376459561fe
achm <achm6174@gmail.com>
2017-06-04 17:18:57 +0800

    update

---
+++ level_1/main_5_test.py
@@ -0,0 +1,94 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import numpy as np # linear algebra
+np.random.seed(6174)
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import r2_score
+import xgboost as xgb
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers.advanced_activations import PReLU
+from keras.callbacks import EarlyStopping
+from keras.layers.normalization import BatchNormalization
+from sklearn.metrics import mean_squared_error
+
+# read datasets
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+# process columns, apply LabelEncoder to categorical features
+for c in train.columns:
+    if train[c].dtype == 'object':
+        lbl = LabelEncoder()
+        lbl.fit(list(train[c].values) + list(test[c].values))
+        train[c] = lbl.transform(list(train[c].values))
+        test[c] = lbl.transform(list(test[c].values))
+
+# shape
+print('Shape train: {}\nShape test: {}'.format(train.shape, test.shape))
+
+
+##Add decomposed components: PCA / ICA etc.
+from sklearn.decomposition import PCA, FastICA
+from sklearn.decomposition import TruncatedSVD
+n_comp = 12
+
+# tSVD
+tsvd = TruncatedSVD(n_components=n_comp, random_state=6174)
+tsvd_results_train = tsvd.fit_transform(train.drop(["y"], axis=1))
+tsvd_results_test = tsvd.transform(test)
+
+# PCA
+pca = PCA(n_components=n_comp, random_state=6174)
+pca2_results_train = pca.fit_transform(train.drop(["y"], axis=1))
+pca2_results_test = pca.transform(test)
+
+# ICA
+ica = FastICA(n_components=n_comp, random_state=6174, tol=1)
+ica2_results_train = ica.fit_transform(train.drop(["y"], axis=1))
+ica2_results_test = ica.transform(test)
+
+# Append decomposition components to datasets
+for i in range(1, n_comp+1):
+    train['pca_' + str(i)] = pca2_results_train[:,i-1]
+    test['pca_' + str(i)] = pca2_results_test[:, i-1]
+
+    train['ica_' + str(i)] = ica2_results_train[:,i-1]
+    test['ica_' + str(i)] = ica2_results_test[:, i-1]
+
+#    train['tsvd_' + str(i)] = tsvd_results_train[:,i-1]
+#    test['tsvd_' + str(i)] = tsvd_results_test[:, i-1]
+
+y_train = train["y"]
+y_mean = np.mean(y_train)
+
+## neural net
+def nn_model():
+    model = Sequential()
+    model.add(Dense(400, input_dim = train.drop(["y"], axis=1).shape[1], kernel_initializer = 'he_normal'))
+    model.add(PReLU())
+    #model.add(Dropout(0.1))
+    model.add(Dense(100, kernel_initializer = 'he_normal'))
+    model.add(PReLU())
+    #model.add(Dropout(0.1))
+    model.add(Dense(1, kernel_initializer = 'he_normal'))
+    model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+    return(model)
+
+model = nn_model()
+pred = np.array([0. for x in range(len(test))])
+num_bags = 5
+for i in range(num_bags):
+    print "#" + str(i)
+    model.fit(train.drop(["y"], axis=1).values,
+                    y_train,
+                    batch_size=32,
+                    epochs = 200,
+                    verbose = 0)
+    pred += model.predict(test.values)[:,0]
+pred = pred/num_bags
+output = pd.DataFrame({'ID': test['ID'].astype(np.int32), 'y': pred})
+output.to_csv('level_1/cache/test/main_5.csv', index=False)
--- level_2/main_4.py
+++ level_2/main_4.py
@@ -7,40 +7,43 @@ df_train = pd.read_csv('data/train.csv')
 df_1 = pd.read_csv("level_1/cache/train/main_1.csv")
 df_2 = pd.read_csv("level_1/cache/train/main_2.csv")
 df_3 = pd.read_csv("level_1/cache/train/main_3.csv")
+df_4 = pd.read_csv("level_1/cache/train/main_4.csv")
 
-r2_score(df_train.y.values, (df_1.y.values))
-r2_score(df_train.y.values, (df_2.y.values))
-r2_score(df_train.y.values, (df_3.y.values))
+print r2_score(df_train.y.values, (df_1.y.values))
+print r2_score(df_train.y.values, (df_2.y.values))
+print r2_score(df_train.y.values, (df_3.y.values))
+print r2_score(df_train.y.values, (df_4.y.values))
 
 from scipy.optimize import minimize
 
 # ======================== NN optimize ======================== #
 def f(coord,args):
-    pred_1, pred_2, pred_3 = args
-    return -r2_score(df_train.y.values, coord[0]*(df_1.y.values) + coord[1]*(df_2.y.values) + coord[2]*(df_3.y.values) )
+    pred_1, pred_2, pred_3, pred_4 = args
+    return -r2_score(df_train.y.values, coord[0]*(df_1.y.values) + coord[1]*(df_2.y.values) + coord[2]*(df_3.y.values) + coord[3]*(df_4.y.values))
 
 
-initial_guess = np.array([1/3. for x in range(3)])
+initial_guess = np.array([1/4. for x in range(4)])
 
 
 res = minimize(f,initial_guess,args = [
                                       df_1.y.values,
                                       df_2.y.values,
-                                      df_3.y.values
+                                      df_3.y.values,
+                                      df_4.y.values
                                      ]
                               ,method='Nelder-Mead')
 print res
 
-tmp = (res.x[0]*(df_1.y.values)  + res.x[1]*(df_2.y.values) + res.x[2]*(df_3.y.values))
+tmp = res.x[0]*(df_1.y.values)  + res.x[1]*(df_2.y.values) + res.x[2]*(df_3.y.values)  + res.x[3]*(df_4.y.values)
 print r2_score(df_train.y.values, tmp)
 
 
 df_1_test = pd.read_csv("level_1/cache/test/main_1.csv")
 df_2_test = pd.read_csv("level_1/cache/test/main_2.csv")
-df_3_test = pd.read_csv("level_1/cache/test/main_3.csv")
-tmp_test = (res.x[0]*(df_1_test.y.values)  + res.x[1]*(df_2_test.y.values) + res.x[2]*(df_3_test.y.values))
+df_4_test = pd.read_csv("level_1/cache/test/main_4.csv")
+tmp_test = (res.x[0]*(df_1_test.y.values)  + res.x[1]*(df_2_test.y.values) + res.x[2]*(df_4_test.y.values))
 
 df_test = df_1_test.copy()
 df_test['y'] = tmp_test
 
-df_test.to_csv("level_2/cache/test/main_2.csv", index=False)
+df_test.to_csv("level_2/cache/test/main_3.csv", index=False)
