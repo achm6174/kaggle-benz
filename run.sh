e1643fa8635fbc8888cdb5a34cb263930178170f
achm6174 <achm6174@gmail.com>
2020-07-26 19:21:55 +0800

    updated

---
+++ run.sh
@@ -0,0 +1,27 @@
+python stacking/main.py
+python level_0/main.py
+python level_1/main_1_train.py
+python level_1/main_1_test.py
+python level_1/main_2_train.py
+python level_1/main_2_test.py
+python level_1/main_3_train.py
+python level_1/main_3_test.py
+python level_1/main_4_train.py
+python level_1/main_4_test.py
+python level_1/main_5_train.py
+python level_1/main_5_test.py
+python level_1/main_6_train.py
+python level_1/main_6_test.py
+python level_1/main_7_train.py
+python level_1/main_7_test.py
+python level_1/main_8_train.py
+python level_1/main_8_test.py
+python level_2/main_1.py
+python level_2/main_2.py
+python level_2/main_3.py
+python level_2/main_4.py
+python level_2/main_5.py
+python level_2/main_6.py
+
+
+
