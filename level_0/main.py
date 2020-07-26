9979181371bd727a745ff2a6a75c4a6bfda4127e
achm6174 <achm6174@gmail.com>
2020-07-26 19:19:43 +0800

    updated

--- level_0/main.py
+++ level_0/main.py
@@ -14,23 +14,21 @@ for i in feat_col:
     if (i == 'y') or (i == 'ID'):
         continue
     test[i] = [x in train[i].values for x in test[i]]
-    #break
 
-print np.sum(test.drop('ID', axis=1).apply(np.sum, 1)!=376)
-print np.sum(test.drop('ID', axis=1).apply(np.sum, 1)==376)
-#train[i].values
+print(np.sum(test.drop('ID', axis=1).apply(np.sum, 1)!=376))
+print(np.sum(test.drop('ID', axis=1).apply(np.sum, 1)==376))
 
 
 train['y'].shift(4)
 train = pd.read_csv('data/train.csv')
 train['y'].shift(1)
-print np.corrcoef(train['y'][1:-1], train['y'].shift(1)[1:-1])
-print np.corrcoef(train['y'][2:-2], train['y'].shift(2)[2:-2])
-print np.corrcoef(train['y'][3:-3], train['y'].shift(3)[3:-3])
-print np.corrcoef(train['y'][4:-4], train['y'].shift(4)[4:-4])
-print np.corrcoef(train['y'][5:-5], train['y'].shift(5)[5:-5])
-print np.corrcoef(train['y'][1:-1], train['y'].shift(-1)[1:-1])
-print np.corrcoef(train['y'][2:-2], train['y'].shift(-2)[2:-2])
-print np.corrcoef(train['y'][3:-3], train['y'].shift(-3)[3:-3])
-print np.corrcoef(train['y'][4:-4], train['y'].shift(4)[4:-4])
-print np.corrcoef(train['y'][5:-5], train['y'].shift(4)[5:-5])
+print(np.corrcoef(train['y'][1:-1], train['y'].shift(1)[1:-1]))
+print(np.corrcoef(train['y'][2:-2], train['y'].shift(2)[2:-2]))
+print(np.corrcoef(train['y'][3:-3], train['y'].shift(3)[3:-3]))
+print(np.corrcoef(train['y'][4:-4], train['y'].shift(4)[4:-4]))
+print(np.corrcoef(train['y'][5:-5], train['y'].shift(5)[5:-5]))
+print(np.corrcoef(train['y'][1:-1], train['y'].shift(-1)[1:-1]))
+print(np.corrcoef(train['y'][2:-2], train['y'].shift(-2)[2:-2]))
+print(np.corrcoef(train['y'][3:-3], train['y'].shift(-3)[3:-3]))
+print(np.corrcoef(train['y'][4:-4], train['y'].shift(4)[4:-4]))
+print(np.corrcoef(train['y'][5:-5], train['y'].shift(4)[5:-5]))
